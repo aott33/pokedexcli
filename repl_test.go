f6385d2afbc5330d9e998ef71b8f4a4d22f17efd
aott33 <ott.andrew33@gmail.com>
2025-11-28 16:47:08 -0800

    continue test development

--- repl_test.go
+++ repl_test.go
@@ -9,6 +9,34 @@ func TestCleanInput(t *testing.T) {
 	case := []struct {
 		input	string
 		expect	[]string
+	}{
+		{
+			input:		" hello world ",
+			expected:	[]string{"hello", "world"}
+		},
+		{
+			input:	"test this string",
+			expected:	[]string{"test", "this", "string"}
+		},
+		{
+			input:	"check   this   spaced  out  string  "},
+			expected:	[]string{"check", "this", "spaced", "out", "string"}
+		}
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		
+		if len(actual) != len(c.expected) {
+			t.Errorf("Length Error: actual and expected are not equal lengths")
+		}
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			// Check each word in the slice
+			// if they don't match, use t.Errorf to print an error message
+			// and fail the test
+		}
 	}
 
 }
